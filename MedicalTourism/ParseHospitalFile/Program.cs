7a14478eb0e116c8d968e4a6e877f0709f218ca5
alishdev <alish.dev@hotmail.com>
2024-09-08 19:56:37 -0400

    Update solution and add new projects and configurations
    - Modified `MedicalTourism.sln`:
      - Changed project type GUID for "ParseAnthemFile".
      - Added new project "ParseHospitalFile" with configurations.
    
    - Updated `AnthemFileParser.cs`:
      - Added `skip` variable in `GetAllFileSizes` method.
    
    - Added `ParseAnthemFile.csproj.user`:
      - Included last selected publish profile.
    
    - Added `FolderProfile.pubxml`:
      - Defined publish profile for "ParseAnthemFile".
    
    - Added `FolderProfile.pubxml.user`:
      - Included user-specific publish history.
    
    - Added `ParseHospitalFile.csproj`:
      - Defined project configuration for .NET 8.0.
    
    - Added `Program.cs` for "ParseHospitalFile":
      - Main entry point prints "Hello, World!".

---
+++ MedicalTourism/ParseHospitalFile/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParseHospitalFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
