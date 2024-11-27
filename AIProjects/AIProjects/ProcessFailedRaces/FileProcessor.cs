7ad13c192cc8f6613d46b16cf3ebc84f94a9cbb3
alishdev <alish.dev@hotmail.com>
2024-11-26 21:39:44 -0500

    more ways to reprocess errors

--- AIProjects/AIProjects/ProcessFailedRaces/FileProcessor.cs
+++ AIProjects/AIProjects/ProcessFailedRaces/FileProcessor.cs
@@ -4,7 +4,14 @@ namespace ProcessFailedRaces;
 
 public class FileProcessor
 {
-    public void ProcessRace(string path, string innerContent)
+    public void ProcessRace(string path)
+    {
+        string innerContent = ReadContentCleanup(path);
+        File.WriteAllText(path: Path.ChangeExtension(path:path, extension:".json"), contents:innerContent);
+        File.Delete(path);
+    }
+
+    private string Cleanup(string innerContent)
     {
         if (innerContent.StartsWith('\"'))
         {
@@ -15,10 +22,69 @@ public class FileProcessor
 
         // replace \" with "
         innerContent = innerContent.Replace("\\\"", "\"").Replace("\\n", "").Replace("\\r", "");
+        return innerContent;
+    }
+
+    private string ReadContentCleanup(string path)
+    {
+        string innerContent = File.ReadAllText(path);
+        return Cleanup(innerContent);
+    }
+
+    public void ToJson(string path)
+    {
+        var exceptionSettings = new JsonSerializerSettings { Error = (se, ev) => { ev.ErrorContext.Handled = true; } };
+        string innerContent = ReadContentCleanup(path);
+        var json = JsonConvert.DeserializeObject<RunInUSAModel>(innerContent, exceptionSettings);
+        string jsonText = JsonConvert.SerializeObject(json);
         File.WriteAllText(path: Path.ChangeExtension(path:path, extension:".json"), contents:innerContent);
         File.Delete(path);
     }
-    
+
+    public void ProcessJournal(string path)
+    {
+        int count = 0;
+        string dir = Path.GetDirectoryName(path)!;
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string firstline = line;
+                string? json = null;
+                if (line.EndsWith("Error parsing JSON content: {"))
+                {
+                    json = "{";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        json += line;
+                        if (line == "}" || (line.Length == 2 && line[0] == '}' /*&& line[1] == '\u200B'*/))
+                            break;
+                    }
+                }
+                else if (line.Contains("Error parsing JSON content:"))
+                {
+                    int index = line.IndexOf("Error parsing JSON content:");
+                    json = line.Substring(index + "Error parsing JSON content:".Length);
+                }
+                
+                // save the json content to a file
+                if (json != null)
+                {
+                    string[] parts = firstline.Split(new char[] {',' });
+                    int raceid = int.Parse(parts[0]);
+                    Console.WriteLine(raceid);
+                    string cleanupText = Cleanup(json);
+                    File.WriteAllText(path: (Path.Combine(dir, $"{raceid}.html")), contents:cleanupText);
+                    File.Delete(path);
+                    
+                    count++;
+                }
+            }
+        }
+        Console.WriteLine($"count = {count}");
+    }
+
     public class RaceDateDistance
     {
         public DateTime? When { get; set; }
--- AIProjects/AIProjects/ProcessFailedRaces/Program.cs
+++ AIProjects/AIProjects/ProcessFailedRaces/Program.cs
@@ -7,8 +7,23 @@ class Program
         // read journal file and get all races that failed to process
         
         FileProcessor fp = new FileProcessor();
-        string path = "/Users/macmyths/BlazorProjects/AIProjects/AIProjects/BuildRunSiteContent/bin/Debug/net8.0/10028.html";
+        /*string path = "/Users/macmyths/BlazorProjects/AIProjects/AIProjects/BuildRunSiteContent/bin/Debug/net8.0/10028.html";
         string fileContent = File.ReadAllText(path);
-        fp.ProcessRace(path, fileContent);
+        fp.ProcessRace(path, fileContent);*/
+        
+        //string journalPath = "/Users/macmyths/Desktop/temp/1/RunInUSAService1.txt";
+        //fp.ProcessJournal(journalPath);
+        
+        //string htmlPath = "/Users/macmyths/Desktop/temp/2/13837.html";    // when "when\":\"Nov 24, 2024 - Sunday
+        //string htmlPath = "/Users/macmyths/Desktop/temp/2/13852.html";    // \"Races\": {\n        \"Swim\": {\n 
+        //string htmlPath = "/Users/macmyths/Desktop/temp/2/14054.html";
+        
+        //get all files with extension html in the directory
+        string dir = "/Users/macmyths/Desktop/temp/2/";
+        string[] files = Directory.GetFiles(dir, "*.html");
+        foreach (string htmlPath in files)
+        {
+            fp.ToJson(htmlPath);    
+        }
     }
 }
