ce402a51cb80483fd7f04d18a18cae2abcaa0f6f
Alisher <alish.dev@hotmail.com>
2025-07-14 21:18:25 -0400

    switch from WPF to MAUI

--- Librarian/UI/Librarian.FileViewer/App.xaml.cs
+++ Librarian/UI/Librarian.FileViewer/App.xaml.cs
@@ -4,8 +4,7 @@ public partial class App : Application
 {
 	public App()
 	{
-        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXhfdHVQRGJcWEZ3WkRWYEk=");
-        InitializeComponent();
+		InitializeComponent();
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
--- Librarian/UI/Librarian.FileViewer/AppShell.xaml.cs
+++ Librarian/UI/Librarian.FileViewer/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace Librarian.FileViewer;
+﻿using Librarian.FileViewer.Components.Pages;
+
+namespace Librarian.FileViewer;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute(nameof(Components.Pages.FileViewer), typeof(Components.Pages.FileViewer));
 	}
 }
